fa5be23d88f0e693ef2bf7e8a5238f4acb5bec77
Alberto Giuntelli <alberto.giuntelli@gmail.com>
2026-04-03 15:25:19 +0200

    Add orders route

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
+
 import productsRoutes from "./routes/productsRoutes.js";
 import promoRoutes from "./routes/promoRoutes.js";
+import ordersRoutes from "./routes/ordersRoutes.js";   // 🔥 AGGIUNTO
 
 const app = express();
 
@@ -12,6 +14,7 @@ app.use(express.json());
 // Routes
 app.use("/api/products", productsRoutes);
 app.use("/api/promo", promoRoutes);
+app.use("/api/orders", ordersRoutes);   // 🔥 AGGIUNTO
 
 // Porta corretta per Railway
 const PORT = process.env.PORT || 8080;
