9c0684d67c5f14de80d20a2d614c2dae44783c1b
Alberto Giuntelli <alberto.giuntelli@gmail.com>
2026-05-21 09:40:13 +0200

    Fix routes prodotti per nuovo backend unificato

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,16 +1,9 @@
 import express from "express";
-import upload from "../middleware/upload.js";
-import { getProdotti, uploadProdotti, deleteProdotti } from "../controllers/productController.js";
+import { getProducts } from "../controllers/productController.js";
 
 const router = express.Router();
 
 // GET prodotti
-router.get("/", getProdotti);
-
-// UPLOAD prodotti
-router.post("/upload", upload.single("file"), uploadProdotti);
-
-// DELETE prodotti
-router.delete("/delete", deleteProdotti);
+router.get("/", getProducts);
 
 export default router;
